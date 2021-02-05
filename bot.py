a5852e4634e9dc8606f919609fc07b48fb658605
XCRunnerS <68029901+XCRunnerS@users.noreply.github.com>
2021-02-05 12:51:31 -0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,33 @@
+# this is the central bot file
+import discord
+from discord.ext import commands
+import random
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+
+
+
+
+
+
+
+
+
+
+
+bot.run('token')
